a17922d3d3e08e30628b68b944478355c0ea133f
shamimspro <shamimspro@gmail.com>
2023-01-26 10:45:26 +0600

    complete very basic java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,12 @@
 public class App {
 
     public static void main(String[] args) {
-
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+        // System.out.println(arr[0][1]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
     }
 }
---
+++ src/App.js
@@ -0,0 +1,2 @@
+const arr = [10, 12, "Shamim", "Snigdha", "Sneha"];
+console.log(arr[0] + arr[1]);
